13b5b8c617ce8bbe42b64bd9a9ae0809d0307c55
aviralgarg0996 <aviralgarg0996@gmail.com>
2018-02-08 23:14:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aviralgarg0996/labourchowk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aviralgarg0996.labourchowk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
